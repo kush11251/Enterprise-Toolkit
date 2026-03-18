38b00e42e91dcdcc478c6e01c7ab2ff71b950d46
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-19 04:10:37 +0530

    feat: integrate lib/utils/doc.sh

---
+++ lib/utils/doc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Function to print documentation for utils.sh
+print_docs() {
+    echo "Utils Documentation"
+    echo "-------------------"
+    echo "print_message: Print a message"
+    echo "add: Add two numbers"
+}
